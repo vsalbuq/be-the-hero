4dfc5fedfa5ebf4226664e8772928fc095c19eb5
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2020-03-25 19:56:26 -0300

    feat: adds basic structure and backend code :tada:

---
+++ packages/server/src/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+const routes = express.Router();
+
+routes.post("/sessions", SessionController.store);
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.store);
+
+routes.get("/profile", ProfileController.index);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.store);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+module.exports = routes;
