80bebec414a507af95513bd7be79173da60bec1f
Lucas-Severo <lucas_severo@outlook.com.br>
2020-03-27 20:38:20 -0400

    fix: add backend validations

--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Joi, Segments } = require('celebrate'); 
 
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
@@ -7,15 +8,36 @@ const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post("/sessions", SessionController.store);
+routes.post("/sessions", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required().length(8)
+    })
+}), SessionController.store);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.store);
 
-routes.get("/profile", ProfileController.index);
+routes.get("/profile", celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
-routes.get("/incidents", IncidentController.index);
+routes.get("/incidents", celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
 routes.post("/incidents", IncidentController.store);
-routes.delete("/incidents/:id", IncidentController.delete);
+routes.delete("/incidents/:id", celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required().length(8)
+    }).unknown(),
+
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
 
 module.exports = routes;
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const { errors } = require('celebrate');
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.listen(3333);
